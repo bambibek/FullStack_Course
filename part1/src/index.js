4903c8d9b798be9aa4fe15e4b1d16208312cfcfa
Bibek Bam <bibek2bam@gmail.com>
2021-02-19 18:37:09 +0200

    part1_1.3

--- part1/src/index.js
+++ part1/src/index.js
@@ -8,13 +8,14 @@ const Header = (props) => {
   )
 }
 const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
 
-    </div>
+  return (
+    < div >
+      <Part part={props.part1.name} exercise={props.part1.exercises} />
+      <Part part={props.part2.name} exercise={props.part2.exercises} />
+      <Part part={props.part3.name} exercise={props.part3.exercises} />
+
+    </div >
   )
 
 }
@@ -37,6 +38,38 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises={(part1.exercises + part2.exercises + part3.exercises)} />
+
+    </div>
+  )
+
+
+}
+
+
+
+
+/*
+const App = () => {
+  const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -52,6 +85,6 @@ const App = () => {
 
     </div>
   )
-}
+}*/
 
 ReactDOM.render(<App />, document.getElementById('root'))
